8b456d865361321b453a05b3acdd2e59255f6300
Gagan Srai <gagondeep.srai@gmail.com>
2025-04-15 12:57:15 +0100

    Update phaser impl to use ParticleEmitter

--- phaser/src/main.ts
+++ phaser/src/main.ts
@@ -1,27 +1,26 @@
-import { Boot } from "./scenes/Boot";
 import { Game as MainGame } from "./scenes/Game";
 
-import { Game, Types } from "phaser";
+import { Game, type Types } from "phaser";
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config: Types.Core.GameConfig = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  parent: "game-container",
-  backgroundColor: "#000000",
-  scale: {
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-  },
-  physics: {
-    default: "arcade",
-    arcade: {
-      gravity: { y: 0 },
-    },
-  },
-  scene: [MainGame],
+	type: Phaser.AUTO,
+	width: 800,
+	height: 600,
+	parent: "game-container",
+	backgroundColor: "#000000",
+	scale: {
+		mode: Phaser.Scale.FIT,
+		autoCenter: Phaser.Scale.CENTER_BOTH,
+	},
+	physics: {
+		default: "arcade",
+		arcade: {
+			gravity: { x: 0, y: 0 },
+		},
+	},
+	scene: [MainGame],
 };
 
 export default new Game(config);
--- phaser/src/scenes/Game.ts
+++ phaser/src/scenes/Game.ts
@@ -1,71 +1,71 @@
 import { Scene } from "phaser";
 
+const BUNNY_SPAWN_COUNT = 10;
+
 export class Game extends Scene {
-  camera: Phaser.Cameras.Scene2D.Camera;
-  bunnies: Array<Phaser.GameObjects.Image>;
-  bunnyCount: number;
-  bunnyType: number;
-  maxX: number;
-  maxY: number;
-  fpsText: string;
+	camera: Phaser.Cameras.Scene2D.Camera;
+	bunnies: Array<Phaser.GameObjects.Image>;
+	bunnyCount: number;
+	bunnyType: number;
+	maxX: number;
+	maxY: number;
+	debugText: Phaser.GameObjects.Text;
+	private emitter: Phaser.GameObjects.Particles.ParticleEmitter;
 
-  constructor() {
-    super("Game");
-    this.bunnies = [];
-    this.bunnyCount = 0;
-    this.bunnyType = 0;
-    this.maxX = 0;
-    this.maxY = 0;
-  }
+	constructor() {
+		super("Game");
+		this.bunnies = [];
+		this.bunnyCount = 0;
+		this.bunnyType = 0;
+		this.maxX = 0;
+		this.maxY = 0;
+	}
 
-  preload() {
-    this.load.setPath("assets");
-    this.load.spritesheet("bunnys", "bunnys.png", {
-      frameWidth: 26,
-      frameHeight: 37,
-    });
-  }
+	preload() {
+		this.load.setPath("assets");
+		this.load.spritesheet("bunnys", "bunnys.png", {
+			frameWidth: 26,
+			frameHeight: 37,
+		});
+	}
 
-  create() {
-    this.maxX = this.sys.game.config.width;
-    this.maxY = this.sys.game.config.height;
+	create() {
+		this.maxX = this.sys.game.config.width as number;
+		this.maxY = this.sys.game.config.height as number;
 
-    this.fpsText = this.add.text(10, this.maxY - 20, "FPS: 0", {
-      font: "16px Arial",
-      fill: "#ffffff",
-    });
+		this.emitter = this.add.particles(0, 0, "bunnys", {
+			frame: this.bunnyType,
+			frequency: -1,
+			lifespan: 1000 * 3600,
+			gravityY: 300,
+			quantity: 1,
+			emitting: false,
+			bounce: 1,
+			bounds: { x: 0, y: 0, width: this.maxX, height: this.maxY },
+			speedX: { min: -300, max: 300 },
+			speedY: { min: -300, max: 300 },
+		});
 
-    this.input.on("pointerdown", () => this.addBunnies(1000));
-    this.input.on("pointerup", () => {
-      this.bunnyType++;
-      this.bunnyType %= 5;
-    });
-  }
+		this.debugText = this.add.text(10, this.maxY - 20, "FPS: 0 | Bunnies: 0", {
+			font: "16px Arial",
+			color: "#ffffff",
+		});
 
-  addBunnies(num: number) {
-    for (let i = 0; i < num; i++) {
-      const bunny = this.physics.add.image(
-        Phaser.Math.Between(0, this.maxX),
-        Phaser.Math.Between(0, this.maxY),
-        "bunnys",
-        this.bunnyType
-      );
+		this.input.on("pointerup", () => {
+			this.bunnyType++;
+			this.bunnyType %= 5;
+			this.emitter.setEmitterFrame(this.bunnyType);
+		});
+	}
 
-      bunny.setVelocity(
-        Phaser.Math.Between(-300, 300),
-        Phaser.Math.Between(-300, 300)
-      );
-      bunny.setBounce(1, 1);
-      bunny.setCollideWorldBounds(true);
-
-      this.bunnies.push(bunny);
-    }
-
-    this.bunnyCount += num;
-    console.log(`Bunnies: ${this.bunnyCount}`);
-  }
-
-  update() {
-    this.fpsText.setText(`FPS: ${Math.round(this.game.loop.actualFps)}`);
-  }
+	update() {
+		const pointer = this.input.activePointer;
+		if (pointer.isDown) {
+			this.emitter.emitParticleAt(pointer.x, pointer.y, BUNNY_SPAWN_COUNT);
+			this.bunnyCount += BUNNY_SPAWN_COUNT;
+		}
+		this.debugText.setText(
+			`FPS: ${Math.round(this.game.loop.actualFps)} | Bunnies ${this.bunnyCount}`,
+		);
+	}
 }
